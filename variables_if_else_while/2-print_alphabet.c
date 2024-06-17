454fae9bb2c3f8687db9b9b506127bcba7aedc8f
Forget Guillaume <9430@holbertonstudents.com>
2024-06-17 16:03:07 +0200

    added task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet lowercase + newline
+ * Return: 0
+ */
+
+int	main(void)
+{
+	int	c = 0;
+
+	for (c = 'a'; c < '{'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
