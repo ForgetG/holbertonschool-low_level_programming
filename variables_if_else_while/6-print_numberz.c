e12376642a2b727d1fac74a8fb7fdd06fd45f511
Forget Guillaume <9430@holbertonstudents.com>
2024-06-17 16:47:55 +0200

    added task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits numbers base 10
+ * Return: 0
+ */
+
+int	main(void)
+{
+	int	i = 48;
+
+	for (; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
