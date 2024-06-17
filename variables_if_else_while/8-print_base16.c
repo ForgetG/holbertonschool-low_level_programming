120c33de1c9c09be44ca84b64ac3ef56840d0fcf
Forget Guillaume <9430@holbertonstudents.com>
2024-06-17 17:11:01 +0200

    added task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers of base 16.
+ * Return: 0
+ */
+
+int	main(void)
+{
+	int	i = 0;
+
+	for (; i < 16; i++)
+	{
+		if (i < 10)
+			putchar('0' + i);
+		else
+			putchar('A' + i - 10);
+	}
+	putchar('\n');
+	return (0);
+}
