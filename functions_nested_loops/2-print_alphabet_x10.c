92ed1b007be4fe6558b02f243b70445b92df4460
Forget Guillaume <9430@holbertonstudents.com>
2024-06-18 11:27:42 +0200

    added task 2 + prototype print_alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet
+ */
+
+void	print_alphabet_x10(void)
+{
+	int	i = 0;
+	char	c = 'a';
+
+	while (i < 11)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
