35ecccd7223ec4febdfde1fcf24a39c905a3717a
Forget Guillaume <9430@holbertonstudents.com>
2024-06-20 14:02:57 +0200

    modified task11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,18 +6,12 @@ void	print_to_98(int n)
 	if (n < 98)
 	{
 		for (; n < 98; n++)
-		{
-			if(n > 9)
-				printf("%2d, ", n);
-			else
-				printf("%d, ", n);
-		}
-	}
-	if (n > 98)
-	{
-		for (; n > 98; n--)
 			printf("%d, ", n);
 	}
-	else 
-		printf("%d\n", n);
+	else
+	{
+		for (; n >= 98; n--)
+			printf("%d, ", n);
+	}
+	printf("%d\n", 98);
 }
