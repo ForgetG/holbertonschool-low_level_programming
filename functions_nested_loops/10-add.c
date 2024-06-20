ed7643a153c7a0410caca0d33fe7af839c417bb5
Forget Guillaume <9430@holbertonstudents.com>
2024-06-20 13:29:07 +0200

    addd task10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @number1: number1
+ * @number2: number2
+ * Return: result of addition
+ */
+
+int	add(int number1, int number2)
+{
+	return (number1 + number2);
+}
