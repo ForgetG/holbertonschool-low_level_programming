aa5aea16a2e767cb7afa3c99ca57660e90308819
Forget Guillaume <9430@holbertonstudents.com>
2024-06-23 10:16:33 +0200

    added 102-print_comb.c

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - print combination of 2 two-digit numbers.
+ * main - print 2 two-digit numbers
  * Return: 0
  */
 
 int	main(void)
 {
-	int	nbr1;
-	int	nbr2;
-	int	nbr3;
-	int	nbr4;
+	int	number1;
+	int	number2;
 
-	nbr1 = '0';
-	nbr2 = '0';
-	nbr3 = '0';
-	nbr4 = '1';
-	while (nbr1 <= '9')
+	for (number1 = 0 ; number1 < 100; number1++)
 	{
-		nbr2 = '0';
-		while (nbr2 <= '8')
+		for (number2 = number1 + 1; number2 < 100; number2++)
 		{
-			nbr3 = nbr1;
-			while (nbr3 <= '9')
+			putchar(number1 / 10 + '0');
+			putchar(number1 % 10 + '0');
+			putchar(' ');
+			putchar(number2 / 10 + '0');
+			putchar(number2 % 10 + '0');
+			if (number1 < 98 || number2 < 99)
 			{
-				nbr4 = nbr2 + 1;
-				while (nbr4 <= '9')
-				{
-					putchar(nbr1);
-					putchar(nbr2);
-					putchar(' ');
-					putchar(nbr3);
-					putchar(nbr4);
-					if (nbr1 == '9' && nbr2 == '8' && nbr3 == '9' && nbr4 == '9')
-						putchar('\n');
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					nbr4++;
-				}
-				nbr3++;
+				putchar(',');
+				putchar(' ');
 			}
-			nbr2++;
 		}
-		nbr1++;
 	}
+	putchar('\n');
 	return (0);
 }
