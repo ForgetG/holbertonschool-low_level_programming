251163880487fd93134806bd3251ae299fdcc0cd
Forget Guillaume <9430@holbertonstudents.com>
2024-06-23 09:08:48 +0200

    modified 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,8 +6,9 @@
 
 int	main(void)
 {
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int	FAILURE = '0';
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, message, 60);
 	return (FAILURE);
 }
