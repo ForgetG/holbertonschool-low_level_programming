4a7c47dd79b004641b7475f6be16fc9c6cf26bc2
Forget Guillaume <9430@holbertonstudents.com>
2024-06-23 11:14:51 +0200

    spacing problem fixed

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -38,7 +38,7 @@ void	print_times_table(int n)
 			else if (result >= 100)
 			{
 				_putchar((result / 100) + '0');
-				_putchar(((result / 10) %10) + '0');
+				_putchar(((result / 10) % 10) + '0');
 				_putchar((result % 10) + '0');
 			}
 			else if (result < 10 && j == 0)
