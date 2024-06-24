67a536a50fcb3c8da3f80ac90c6be252f67825c3
Forget Guillaume <9430@holbertonstudents.com>
2024-06-24 10:23:46 +0200

    modified datatypes of number1 and number 2

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -11,11 +11,11 @@ int	main(void)
 	size_t	number1 = 1, number2 = 2, number3;
 	int	index = 3;
 
-	printf("%u, %u", number1, number2);
+	printf("%lu, %lu", number1, number2);
 	for (; index <= 98; index++)
 	{
 		number3 = number1 + number2;
-		printf(", %u", number3);
+		printf(", %lu", number3);
 		number1 = number2;
 		number2 = number3;
 	}
