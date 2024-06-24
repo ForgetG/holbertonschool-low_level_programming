017ef8d45db3e1aae614c0af78a4171e695f1d18
Forget Guillaume <9430@holbertonstudents.com>
2024-06-24 12:06:55 +0200

    added 7-print_diagonal.c & updated prototypes in main.h

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of lines
+ */
+
+void	print_diagonal(int n)
+{
+	int	i;
+	int	j;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('\\');
+		_putchar('\n');
+		if (i == n)
+			break;
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+	}
+	if (n >= 0)
+		_putchar('\n');
+}
