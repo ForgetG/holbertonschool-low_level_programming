1d34903bdd56e4f3bbcec06a1d2b5ee6f4c9dafe
Forget Guillaume <9430@holbertonstudents.com>
2024-06-24 16:38:56 +0200

    added task 8 + added prototype in main.h

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n element of an array
+ * @a: array
+ * @n: n elements of array
+ */
+
+void	print_array(int *a, int n)
+{
+	int	i = 0;
+
+	for (; i < n; i++)
+	{
+		if (i < n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+}
