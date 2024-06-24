5735dd41a70e09b46b6e2fd221053b3c59ac10e1
Forget Guillaume <9430@holbertonstudents.com>
2024-06-24 16:45:49 +0200

    added if condition

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,14 @@ void	print_array(int *a, int n)
 {
 	int	i = 0;
 
-	for (; i < n; i++)
+	if (n > 1)
 	{
-		if (i < n - 1)
-			printf("%d, ", a[i]);
-		else
-			printf("%d\n", a[i]);
+		for (; i < n; i++)
+		{
+			if (i < n - 1)
+				printf("%d, ", a[i]);
+			else
+				printf("%d\n", a[i]);
+		}
 	}
 }
