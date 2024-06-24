2d4fc2775c026b954df23cdb695b5dfe083a5025
Forget Guillaume <9430@holbertonstudents.com>
2024-06-24 13:48:19 +0200

    added 9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print number from 1 to 100 with fizzbuzz
+ * Return: 0
+ */
+
+int	main(void)
+{
+	int	i, tens, units;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i > 9)
+		{
+			tens = i / 10;
+			units = i % 10;
+			printf("%d%d", tens, units);
+		}
+		else if (i < 9)
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
