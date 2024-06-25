830f8a6fc8649c16bf833ce6796a29eb6c21b594
Forget Guillaume <9430@holbertonstudents.com>
2024-06-25 16:33:53 +0200

    added 7-leet.c + added prototype in main.h

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encode a string to 1337
+ * @s : string to encode
+ * Return: string
+ */
+
+char	*leet(char *s)
+{
+	int	i = 0;
+	int	j;
+	char	l[8] = {'O', 'L', '9', 'E', 'A', '9', '9', 'T'};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j <= 7;j++)
+		{
+			if (s[i] == l[j] || s[i] - 32 == l[j])
+				s[i] = j + '0';
+		}
+		i++;
+	}
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ char	*_strncat(char *dest, char *src, int n);
 char	*_strncpy(char *dest, char *src, int n);
 char	*string_toupper(char *);
 char	*cap_string(char *);
+char	*leet(char *);
 
 void	reset_to_98(int	*n);
 void	swap_int(int *a, int *b);
