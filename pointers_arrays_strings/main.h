a7dad01055c4aa0c757f5c9f24333a83111d0c1a
Forget Guillaume <9430@holbertonstudents.com>
2024-06-25 10:44:10 +0200

    added 0-strcat + added prototype in main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ int	_strlen(char *s);
 int	_atoi(char *s);
 
 char	*_strcpy(char *dest, char *src);
+char	*_strcat(char *dest, char *src);
 
 void	reset_to_98(int	*n);
 void	swap_int(int *a, int *b);
