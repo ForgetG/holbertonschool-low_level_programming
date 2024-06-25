88da0e18e75c5a577abc04026bb3476ea68cd91e
Forget Guillaume <9430@holbertonstudents.com>
2024-06-25 14:31:24 +0200

    took in count integers in 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char	*cap_string(char *s)
 	i++;
 	while (s[i] != '\0')
 	{
-		if (s[i - 1] < 'A' || s[i - 1] > 'z')
+		if (s[i - 1] < '0' || (s[i - 1] > '9' && s[i - 1] < 'A') || s[i - 1] > 'z')
 		{
 			if (s[i] >= 'a' && s[i] <= 'z')
 				s[i] -= 32;
