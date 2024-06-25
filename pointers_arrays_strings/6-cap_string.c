a2b2ae786a7817689a01d9423b205d817bb7da37
Forget Guillaume <9430@holbertonstudents.com>
2024-06-25 14:26:29 +0200

    added 6-cap_string.c + added prototype in main.h

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of a string
+ * @s : string
+ * Return: s
+ */
+
+char	*cap_string(char *s)
+{
+	int	i = 0;
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] -= 32;
+	i++;
+	while (s[i] != '\0')
+	{
+		if (s[i - 1] < 'A' || s[i - 1] > 'z')
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
