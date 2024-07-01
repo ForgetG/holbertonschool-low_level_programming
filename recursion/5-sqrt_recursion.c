b71e7aacf9f2f0990444f8f0980fd9c71d384e5a
Forget Guillaume <9430@holbertonstudents.com>
2024-07-01 15:57:19 +0200

    new code

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * helper - calculate square root
+ * @n : number
+ * @min: minimum possible square root
+ * @max: maximum square root
+ * Return: sqrt or -1
+ */
+
+int	helper(int n, int min, int max)
+{
+	int	sqrt;
+
+	if (max < min)
+		return (-1);
+	sqrt = (min + max) / 2;
+	if (sqrt * sqrt == n)
+		return (sqrt);
+	else if (sqrt * sqrt < n)
+		return (helper(n, sqrt + 1, max));
+	else
+		return (helper(n, min, sqrt - 1));
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number
  * Return: sqrt or 0
@@ -8,21 +31,7 @@
 
 int	_sqrt_recursion(int n)
 {
-	int	nb2;
-	int	sqrt;
-
-	sqrt = 1;
-	nb2 = sqrt * sqrt;
-	if (n > 0)
-	{
-		while (sqrt <= 46341 && nb2 < n)
-		{
-			sqrt++;
-			nb2 = sqrt * sqrt;
-		}
-		if (nb2 == n)
-			return (sqrt);
-	}
-	return (-1);
+	if (n < 0)
+		return (-1);
+	return (helper(n, 1, n));
 }
-
