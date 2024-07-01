9ff479d09373cf016c6a9df106d28a01f7c550fd
Forget Guillaume <9430@holbertonstudents.com>
2024-07-01 12:42:58 +0200

    modified code to for loop

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,9 @@
 
 char	*_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n != 0)
-	{
-		dest[n] = src[n];
-		n--;
-	}
+	unsigned int	i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
