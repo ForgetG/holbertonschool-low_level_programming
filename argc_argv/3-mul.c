61187828e5020d07d9089e9fe2cbe96575da2351
Forget Guillaume <9430@holbertonstudents.com>
2024-07-02 11:22:04 +0200

    created own atoi function

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * _atoi - converts char to integer
+ * @str: string to convert
+ * Return: result
+ */
+
+int	_atoi(char *str)
+{
+	int	result = 0;
+	int	sign = 1;
+	int	start = 0;
+	int	i = 0;
+
+	if (str[0] == '-')
+	{
+		sign = -1;
+		start = 1;
+	}
+	for (i = start; str[i] != '\0'; ++i)
+		result = result * 10 + str[i] - '0';
+	return (result * sign);
+}
 
 /**
  * main - multiplies two numbers.
@@ -17,8 +39,8 @@ int	main(int argc, char **argv)
 		printf("ERROR\n");
 		return (1);
 	}
-	mul1 = atoi(argv[1]);
-	mul2 = atoi(argv[2]);
+	mul1 = _atoi(argv[1]);
+	mul2 = _atoi(argv[2]);
 	result = mul1 * mul2;
 	printf("%d\n", result);
 	return (0);
