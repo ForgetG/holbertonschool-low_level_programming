73290a47ae76c4ae19fe126da140cbe9f27d3137
Forget Guillaume <9430@holbertonstudents.com>
2024-07-02 14:52:53 +0200

    added 100-is_palindrome.c + prototype in main.h

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * check_palindrome - check for palindrome
+ * @s: string to check
+ * @start: index start of string
+ * @end: index end of string
+ * Return: 0 not a palindrome or 1 is a palindrome
+ */
+
+int	check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (check_palindrome(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - calculates length of string then call check_palindrome
+ * @s: string to calculate
+ * Return: is_palindrome
+ */
+
+int	is_palindrome(char *s)
+{
+	int	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (check_palindrome(s, 0, i - 1));
+}
