2eb1c75448a96eeae69f252706b2a9d6525f07eb
Forget Guillaume <9430@holbertonstudents.com>
2024-07-08 15:39:39 +0200

    added 4-free_grid.c + prototype in main.h

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * free_grid - free allocated memory for grid
+ * @grid: 2 dimensional grid
+ * @height: height of the grid
+ */
+
+void	free_grid(int **grid, int height)
+{
+	int	index;
+
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+	free(grid);
+}
