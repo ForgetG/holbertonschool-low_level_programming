194947414bf0935b995f6c6e6056a3fb8f451789
Forget Guillaume <9430@holbertonstudents.com>
2024-07-08 11:42:56 +0200

    added 0-create_array + main.h

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * create_array - create an array of chars inizialized with specific char.
+ * @size: size of array
+ * @c: specific char
+ * Return: array or NULL
+ */
+
+char	*create_array(unsigned int size, char c)
+{
+	unsigned int	index;
+	char	*array;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+	for (index = 0; index < size; index++)
+		array[index] = c;
+	return (array);
+}
