5d95b7879964fa910a7e98b0c9e4d9f3b0309ca0
Forget Guillaume <9430@holbertonstudents.com>
2024-07-09 09:53:50 +0200

    added dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define dog.
+ * @name: name of dog.
+ * @owner: name of owner.
+ * @age: age of dog.
+ */
+
+struct dog
+{
+	char	*name;
+	char	*owner;
+	float	age;
+};
+
+#endif
