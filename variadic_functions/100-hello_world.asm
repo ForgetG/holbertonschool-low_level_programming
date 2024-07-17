399927770152d11fecac3e94add6ca08cfc5d6a0
Forget Guillaume <9430@holbertonstudents.com>
2024-07-17 10:28:52 +0200

    modify assembly code: change _start to main / removed explicit exit syscall

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -3,15 +3,13 @@ section .data
 	len equ $ - msg			; length of the string
 
 section .text
-	global _start			; must be declared for linker (ld)
+	global main			; must be declared for linker (ld)
 
-_start:
+main:
 	mov rax, 1			; system call number (sys_write)
 	mov rdi, 1			; file descriptor (stdout)
 	mov rsi, msg			; pointer to message to write
 	mov rdx, len			; length of message
 	syscall				; call kernel
 
-	mov rax, 60			; system call number (sys_exit)
-	xor rdi, rdi			; exit code 0
-	syscall				; call kernel
+	;no need for an explicit exit syscall; C runtime will handle it.
