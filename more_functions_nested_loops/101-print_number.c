65b5391505c4b148f9ca7e94d8acbb508a1c8e8b
Forget Guillaume <9430@holbertonstudents.com>
2024-07-19 10:26:29 +0200

    added unsigned int variable to hold value of INT_MIN

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to print
+ * print_number - prints an integer using putchar.
+ * @n: given number.
  */
 
 void	print_number(int n)
 {
+	unsigned int number;
 	if (n < 0)
 	{
+
 		_putchar('-');
-		n = -n;
+		number = -n;
 	}
-	if (n / 10 != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	else
+		number = n;
+	if (number / 10 != 0)
+		print_number(number / 10);
+	_putchar((number % 10) + '0');
 }
