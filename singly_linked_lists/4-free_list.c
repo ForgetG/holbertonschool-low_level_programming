e36a009a48a976ac77f4267d9a9e7e869acc5c03
Forget Guillaume <9430@holbertonstudents.com>
2024-07-29 16:17:02 +0200

    added task 4: free_list + prototype in lists.h

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a malloced list_t list.
+ * @head: head of list_t list.
+ */
+
+void	free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
