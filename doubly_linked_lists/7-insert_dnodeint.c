dd06d50f5a2b0ea1b076fa43ed5018ad0ba2d891
Forget Guillaume <9430@holbertonstudents.com>
2024-07-30 10:20:45 +0200

    added task 7: Insert at index + added prototype in Header file

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * *insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Given dlistint_t list.
+ * @idx: Given index.
+ * @n: Data (integer) for new node.
+ * Return: address of the new_node, or NULL if it failed.
+ */
+
+dlistint_t	*insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t	*new_node;
+	dlistint_t	*current_node = *h;
+	unsigned int	index = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+	}
+	else
+	{
+		while (current_node != NULL && index < idx)
+		{
+			if (index == idx - 1)
+			{
+				new_node->next = current_node->next;
+				new_node->prev = current_node;
+				if (current_node->next != NULL)
+					current_node->next->prev = new_node;
+				current_node->next = new_node;
+				return (new_node);
+			}
+			current_node = current_node->next;
+			index++;
+		}
+		free(new_node);
+		return (NULL);
+	}
+	return (new_node);
+}
