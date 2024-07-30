dabec71149ab6f8f03ec92f754f6d300d6d2d57f
Forget Guillaume <9430@holbertonstudents.com>
2024-07-30 07:10:59 +0200

    new approach to task 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,16 +13,23 @@ dlistint_t	*add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t	*current_node;
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
-	new_node->prev = NULL;
 	new_node->next = NULL;
 
 	if (*head == NULL)
-		return (NULL);
-	current_node = *head;
-	while (current_node->next != NULL)
-		current_node = current_node->next;
-	current_node->next = new_node;
-	new_node->prev = current_node;
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		current_node = *head;
+		while (current_node->next != NULL)
+			current_node = current_node->next;
+		current_node->next = new_node;
+		new_node->prev = current_node;
+	}
 	return (new_node);
 }
