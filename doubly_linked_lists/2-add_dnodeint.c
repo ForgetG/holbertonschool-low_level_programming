5fec176f96945949c603523b48056bb6f04fa3fb
Forget Guillaume <9430@holbertonstudents.com>
2024-07-30 06:04:50 +0200

    added task 2: Add node (2-add_dnodeint.c) + added function prototype in lists.h

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * *add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: head of given dlistint_t list.
+ * @n: data to add in new node.
+ * Return: address of new element, or NULL if it failed.
+ */
+
+dlistint_t	*add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t	*new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
