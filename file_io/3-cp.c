15570d3c62a9cbe3caed0269c121f2438d355950
Forget Guillaume <9430@holbertonstudents.com>
2024-08-01 20:17:39 +0200

    removed all previous change

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,54 +1,37 @@
 #include "main.h"
 
 /**
- * read_and_write - Reads from src_fd and writes to dest_fd.
+ * copy_file_content - Copies the content from one file descriptor to another.
  * @src_fd: The source file descriptor.
  * @dest_fd: The destination file descriptor.
- * @buffer: The buffer to use for reading and writing.
- *
- * Return: On success: 1.
- * On failure: -1.
+ * @src_filename: The source file name.
+ * @dest_filename: The destination file name.
  */
-int read_and_write(int src_fd, int dest_fd, char *buffer)
+
+void	copy_file_content(int src_fd, int dest_fd,
+		const char *src_filename, const char *dest_filename)
 {
-	int n_read, n_written;
+	int	n_read, n_written;
+	char	buffer[1024];
 
 	while ((n_read = read(src_fd, buffer, 1024)) > 0)
 	{
 		n_written = write(dest_fd, buffer, n_read);
 		if (n_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
-			return (-1);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_filename);
+			close(src_fd);
+			close(dest_fd);
+			exit(99);
 		}
 	}
 	if (n_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
-		return (-1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_filename);
+		close(src_fd);
+		close(dest_fd);
+		exit(98);
 	}
-	return (1);
-}
-
-/**
- * copy_file_content - Copies the content from one file descriptor to another.
- * @src_fd: The source file descriptor.
- * @dest_fd: The destination file descriptor.
- *
- * Return: On success: 1.
- * On failure: -1.
- */
-int copy_file_content(int src_fd, int dest_fd)
-{
-	char *buffer = malloc(1024);
-	int result;
-
-	if (buffer == NULL)
-		return (-1);
-
-	result = read_and_write(src_fd, dest_fd, buffer);
-	free(buffer);
-	return (result);
 }
 
 /**
@@ -59,45 +42,39 @@ int copy_file_content(int src_fd, int dest_fd)
  * Return: On success: 1.
  * On failure: -1.
  */
-int copy_file(const char *src_filename, const char *dest_filename)
+
+int	copy_file(const char *src_filename, const char *dest_filename)
 {
-	int src_fd, dest_fd;
-	int result;
+	int	src_fd, dest_fd;
 
 	if (src_filename == NULL || dest_filename == NULL)
 		return (-1);
-
 	src_fd = open(src_filename, O_RDONLY);
 	if (src_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_filename);
-		return (-1);
+		exit(98);
 	}
-
 	dest_fd = open(dest_filename, O_CREAT | O_WRONLY | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (dest_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_filename);
 		close(src_fd);
-		return (-1);
+		exit(99);
 	}
-
-	result = copy_file_content(src_fd, dest_fd);
-
+	copy_file_content(src_fd, dest_fd, src_filename, dest_filename);
 	if (close(src_fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_fd);
-		result = -1;
+		exit(100);
 	}
-
 	if (close(dest_fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
-		result = -1;
+		exit(100);
 	}
-
-	return (result);
+	return (1);
 }
 
 /**
@@ -108,7 +85,8 @@ int copy_file(const char *src_filename, const char *dest_filename)
  * Return: On success: 0.
  * On failure: Corresponding error code.
  */
-int main(int argc, char **argv)
+
+int	main(int argc, char **argv)
 {
 	umask(0002);
 	if (argc != 3)
@@ -116,11 +94,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	if (copy_file(argv[1], argv[2]) == -1)
-	{
 		exit(1);
-	}
-
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -11,9 +11,9 @@ int	_putchar(char c);
 int	create_file(const char *filename, char *text_content);
 int	append_text_to_file(const char *filename, char *text_content);
 int	copy_file(const char *src_filename, const char *dest_filename);
-int	read_and_write(int src_fd, int dest_fd, char *buffer);
 
-int	copy_file_content(int src_fd, int dest_fd);
+int	copy_file_content(int src_fd, int dest_fd,
+		const char *src_filename, const char *dest_filename);
 
 ssize_t	read_textfile(const char *filename, size_t letters);
 
