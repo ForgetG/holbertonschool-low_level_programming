9f82b0e868be204e62dcc33147c6b0d1e1d90b1c
Forget Guillaume <9430@holbertonstudents.com>
2024-08-02 13:05:39 +0200

    creation of close_file function + added prototype in main.h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,6 +14,20 @@ void	handle_error(int exit_code, const char *message, const char argv[])
 }
 
 /**
+ * close_file - Close a given filed.
+ * @fd: given file
+ */
+
+void	close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - Entry point for the program.
  * @argc: Argument count.
  * @argv: Argument vector.
@@ -36,7 +50,7 @@ int	main(int argc, char *argv[])
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (dest_fd == -1)
 	{
-		close(src_fd);
+		close_file(src_fd);
 		handle_error(99, "Error: Can't write to %s\n", argv[2]);
 	}
 	while ((n_read = read(src_fd, buffer, 1024)) > 0)
@@ -44,20 +58,18 @@ int	main(int argc, char *argv[])
 		n_written = write(dest_fd, buffer, n_read);
 		if (n_written == -1)
 		{
+			close_file(src_fd);
+			close_file(dest_fd);
 			handle_error(99, "Error: Can't write from file %s\n", argv[2]);
 		}
 	}
 	if (n_read == -1)
+	{
+		close_file(src_fd);
+		close_file(dest_fd);
 		handle_error(98, "Error: Can't read from file %s\n", argv[1]);
-	if (close(src_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_fd);
-		exit(100);
 	}
-	if (close(dest_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
-		exit(100);
-	}
+	close_file(src_fd);
+	close_file(dest_fd);
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -12,6 +12,7 @@ int	create_file(const char *filename, char *text_content);
 int	append_text_to_file(const char *filename, char *text_content);
 
 void	handle_error(int exit_code, const char *message, const char argv[]);
+void	close_file(int fd);
 
 ssize_t	read_textfile(const char *filename, size_t letters);
 
