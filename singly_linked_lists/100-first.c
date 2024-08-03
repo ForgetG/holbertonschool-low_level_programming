3d54c179d9f39d56d94fe1afc59e7f60fca4611e
Forget Guillaume <9430@holbertonstudents.com>
2024-08-03 08:10:11 +0200

    Added task 5(bonus): The Hare and the Tortoise(100-first.c) + added prototytpe in header file

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * pre_main - Writes a string before main function.
+ */
+
+void	pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
